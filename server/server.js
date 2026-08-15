bffae1e336d3be1377e7595fe41e76988d7d4168
rajankumar-dev <rk1620703@gmail.com>
2026-08-15 10:43:10 +0530

    add new npm packages and connect mongodb successfully

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
 
+//middlewares
 app.use(express.json());
+app.use(cors());
 
+const port = process.env.PORT || 3000;
+
+//MongoDB connection
+connectDB();
+
+//test route
 app.get("/", (req, res) => {
   res.json({
     message: "Luxe Screen API is running",

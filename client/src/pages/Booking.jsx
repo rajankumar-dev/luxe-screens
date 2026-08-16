424acc5b87950d06c07e114943033b09f36e0227
rajankumar-dev <rk1620703@gmail.com>
2026-08-16 16:21:25 +0530

    feat: add Booking page component

---
+++ client/src/pages/Booking.jsx
@@ -0,0 +1,23 @@
+function Booking() {
+    return (
+        <main className="min-h-screen bg-black px-6 py-16 text-white">
+            <div className="mx-auto max-w-5xl">
+                <p className="text-sm uppercase tracking-[0.3em] text-white/40">
+                    Booking
+                </p>
+
+                <h1 className="mt-3 text-5xl font-bold">
+                    Book your experience.
+                </h1>
+
+                <p className="mt-5 max-w-xl text-white/50">
+                    Complete your booking in a few simple steps.
+                </p>
+
+                {/* Booking steps will be added here */}
+            </div>
+        </main>
+    );
+}
+
+export default Booking;

fdc8dff3db291f5f018781e1525b792e184cf999
rajankumar-dev <rk1620703@gmail.com>
2026-08-17 21:00:36 +0530

    fix: update theater functionality to getTheatertById

--- server/controllers/theaterController.js
+++ server/controllers/theaterController.js
@@ -15,3 +15,27 @@ export const getTheaters = async (req, res) => {
     });
   }
 };
+
+export const getTheaterById = async (req, res) => {
+  try {
+    const theater = await Theater.findById(req.params.id);
+
+    if (!theater) {
+      return res.status(404).json({
+        success: false,
+        message: "Theater not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      theater,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch theater",
+      error: error.message,
+    });
+  }
+};
--- server/routes/theaterRoutes.js
+++ server/routes/theaterRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { getTheaters } from "../controllers/theaterController.js";
+import {
+  getTheaterById,
+  getTheaters,
+} from "../controllers/theaterController.js";
 const router = express.Router();
 
 router.get("/", getTheaters);
+router.get("/:id", getTheaterById);
 
 export default router;

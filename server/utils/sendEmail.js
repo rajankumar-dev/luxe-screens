14b5cc1ed1a9d317e3e2773c74e53a1dd2d54bff
rajankumar-dev <rk1620703@gmail.com>
2026-08-21 17:00:45 +0530

    fix production OTP email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,12 +2,25 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, html) => {
   try {
+    // Check environment variables
+    if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
+      throw new Error(
+        "EMAIL_USER or EMAIL_PASSWORD is missing in environment variables",
+      );
+    }
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
       },
+
+      // Prevent request from hanging for a long time
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
 
     await transporter.sendMail({
@@ -19,7 +32,8 @@ export const sendEmail = async (to, subject, html) => {
 
     console.log(`Email sent successfully to ${to}`);
   } catch (error) {
-    console.error("Email sending failed:", error.message);
+    console.error("Email sending failed:", error);
+
     throw error;
   }
 };
